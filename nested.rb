f97b1020d988d478a5d689d90da53c9ce7de7c6a
neivyx <janeivyhilario@gmail.com>
2020-06-21 11:49:34 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -17,7 +17,9 @@ def hopper
      }
 
 
-end
+grace_hopper = programmer_hash[:grace_hopper]
+
+  end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +39,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+     alan_kay = programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +60,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     dennis_ritchie = programmer_hash[:dennis_ritchie][:languages][0]
+
 end
 
 def adding_matz
@@ -64,8 +71,12 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash = 
+  programmer_hash = 
  		{
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -91,7 +102,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -100,7 +111,14 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+    #  alans_new_info = "GUI"
+
+# changing_alan[:programmer_hash][:alan_kay][:known_for][0] = "GUI"
+
+# programmer_hash[:alan_kay][:known_for][0] = "GUI"
+
+# programmer_hash[:alan_kay][:known_for][0]
+
      
      
 end
@@ -114,12 +132,15 @@ def adding_to_dennis
         },
         :alan_kay => {
           :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
+          :languages => ["Smalltalk", "LISP",]
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["C", "Assembly"]
         }
      }
+
+
+
 
 end
