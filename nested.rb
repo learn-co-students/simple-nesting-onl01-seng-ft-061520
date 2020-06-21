630b6b28fe0b6ba490beaf6354f2155f533458a2
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-21 15:52:18 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -99,7 +99,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "GUI",
+          :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -110,7 +110,8 @@ def changing_alan
 
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
     #  alans_new_info = "GUI"
-
+		programmer_hash[:alan_kay][:known_for] = alans_new_info
+			programmer_hash
 
 
 end
@@ -128,9 +129,10 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C", "Assembly"]
+          :languages => ["C"]
 
         }
      }
+		 
 
 end
